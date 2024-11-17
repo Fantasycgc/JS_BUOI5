2e309c87ec7a95b78686103727a4438bd7b65e3b
hn.tien <hntien248@yahoo.com>
2024-11-17 21:12:13 +0700

    update

--- app/controllers/process.js
+++ app/controllers/process.js
@@ -1,4 +1,17 @@
 function showConnect() {
+
     const customer = document.getElementById("selCustomer").value;
     document.getElementById("inputConnect").style.display = "company" == customer ? "block" : "none"
 }
+document.onkeydown = function (e) {
+    e = e || window.event;
+
+    if (
+        e.keyCode === 123 ||
+        (e.ctrlKey && e.shiftKey && e.keyCode === 73) ||
+        (e.ctrlKey && e.keyCode === 85)
+    ) {
+        e.preventDefault();
+        return false;
+    }
+};
